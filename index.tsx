e77a240819b148b88f7730a85b7941ce89b7bdcf
jnaguinix <jnaguinix@gmail.com>
2025-07-31 01:13:36 -0500

    Fix: Move images to public folder and update paths

--- index.tsx
+++ index.tsx
@@ -3,16 +3,16 @@ import { createRoot } from 'react-dom/client';
 
 // Array de rutas a las imágenes locales que el usuario deberá subir
 const IMAGE_PATHS = [
-  './images/image-1.png',
-  './images/image-2.png',
-  './images/image-3.png',
-  './images/image-4.png',
-  './images/image-5.png',
-  './images/image-6.png',
-  './images/image-7.png',
-  './images/image-8.png',
-  './images/image-9.png',
-  './images/image-10.png',
+  '/images/image-1.png',
+  '/images/image-2.png',
+  '/images/image-3.png',
+  '/images/image-4.png',
+  '/images/image-5.png',
+  '/images/image-6.png',
+  '/images/image-7.png',
+  '/images/image-8.png',
+  '/images/image-9.png',
+  '/images/image-10.png',
 ];
 
 const GRID_ROWS = 5;
